46232fef508607ca54b8e292050db0f835b36522
Kazantsev <ka.yu.8@yandex.ru>
2023-09-04 18:37:04 +0200

    chore: Refactor code to align with project requirements

---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,51 @@
+package ru.yandex.practicum.filmorate.controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+
+    @PostMapping
+    public User save( @RequestBody @Valid User user) {
+        log.info("Invoke save method with user = {}", user);
+        users.put(user.getId(),user);
+        return users.get(user.getId());
+
+    }
+
+    @GetMapping("/{id}")
+    public User get(@PathVariable long id){
+        log.info("Invoke get method for film with id = {}", id);
+        return users.get(id);
+    }
+
+    @GetMapping("/get-all")
+    public List<User> getAllUsers() {
+        log.info("Invoke getAllUsers method");
+        List<User> allUsers = new ArrayList<>(users.values());
+        log.info("Returning {} users: {}", allUsers.size(), allUsers);
+        return allUsers;
+    }
+
+    @PutMapping
+    public User updateFilm(@PathVariable Long userId, @RequestBody @Valid User updatedUser) {
+        if (users.containsKey(userId)) {
+            log.info("Invoke updateFilm method for film with id = {}", userId);
+            users.remove(userId);
+            users.put(updatedUser.getId(),updatedUser);
+            return updatedUser;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private static Long idCounter = 0L;
+    private final Long id;
+
+    @NotBlank
+    private final String name;
+
+    @Size(max = 200)
+    private final String description;
+
+    @NotEmpty
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private final LocalDate releaseDate;
+
+    @Positive
+    private final long duration;
+
+    public Film(String name, String description , LocalDate releaseDate, long duration) {
+        this.id = ++idCounter;
+        this.name = name;
+        this.description = description;
+
+        LocalDate minDate = LocalDate.of(1895, 12, 28);
+        if (releaseDate.isBefore(minDate)) {
+            throw new IllegalArgumentException("Release date cannot be earlier than 28-12-1895");
+        }
+
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
+}
