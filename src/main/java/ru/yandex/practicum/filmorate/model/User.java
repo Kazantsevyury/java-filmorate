a197828c60d775c8a086b86024c93113f51ed0e4
Kazantsev <ka.yu.8@yandex.ru>
2023-09-06 23:54:23 +0200

    File check

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -10,13 +10,13 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
+
 @Data
 public class User {
     private static Long idCounter = 0L;
 
     @NotBlank
     private final String login;
-
     private final String name;
     private final Long id;
 
