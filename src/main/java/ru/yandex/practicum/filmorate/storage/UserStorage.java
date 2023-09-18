53fdb7d95387321a25b415463d0aa2f35f444d50
Kazantsev <ka.yu.8@yandex.ru>
2023-09-18 02:41:34 +0200

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,16 +5,16 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
 import java.util.Set;
 
-public interface UserStorage{
-    User save (User user);
-    User getById (Long id);
-    void  deleteById (Long id);
+public interface UserStorage {
+    User save(User user);
+    User getById(Long id);
+    void deleteById(Long id);
     boolean existenceOfTheUserIdInStorage(Long id);
-    void updateById (Long id, User user);
+    void updateById(Long id, User user);
     Collection<User> getAllUsers();
-    void deleteFriend(Long userId, Long FriendId);
+    void deleteFriend(Long userId, Long friendId);
     User addFriend(Long userId, Long friendId);
-    void addLike (Long userId, Long filmId);
-    void removeLikeFromUser (Long userId, Long filmId);
-    Set<Long> getFriendsSet (Long userId);
+    void addLike(Long userId, Long filmId);
+    void removeLikeFromUser(Long userId, Long filmId);
+    Set<Long> getFriendsSet(Long userId);
 }
