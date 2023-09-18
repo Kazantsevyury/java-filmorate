eb8a18258754d17474364a206b998a6f1c907172
Kazantsev <ka.yu.8@yandex.ru>
2023-09-18 02:27:25 +0200

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -4,17 +4,16 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 
 public interface FilmStorage {
-    Film save (Film film);
+    Film save(Film film);
     Film getById(Long id);
-    void  deleteById (Long id);
-    Collection<Film> getAllFilms() ;
+    void deleteById(Long id);
+    Collection<Film> getAllFilms();
     boolean existenceOfTheFilmIdInStorage(Long id);
-    void updateById (Long id, Film film);
+    void updateById(Long id, Film film);
     Collection<Film> getMostLikedFilms(int limit);
-    Film addLike (Long userId, Long filmId) ;
+    Film addLike(Long userId, Long filmId);
     Collection<Film> getPopularFilms(int count);
     void updateTop();
     List<Film> getTopFilms(int count);
