cdfdad0ecebb10bb893d9ac4ba3c33cfa8ac38fc
Kazantsev <ka.yu.8@yandex.ru>
2023-09-19 23:40:17 +0200

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,11 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.UserService;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FilmorateApplication {
